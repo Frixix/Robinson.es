1cda8a1d33f89d3ef7c4cd462b692ea0385d6dc1
frix <robinsonestibenrodriguez1234@gmail.com>
2026-03-24 14:08:20 -0500

    Cambio de el nombre de la imagen

--- doc_js/script.js
+++ doc_js/script.js
@@ -48,14 +48,13 @@ const proyectos = {
             "img/proyectos/Notestack/pantalla_principal.jpg",
             "img/proyectos/Notestack/cuaderno.jpg",
             "img/proyectos/Notestack/notas.jpg",
-            "img/proyectos/Notestack/cuerpo_Cuaderno.jpg", //img dando problema 
-        
+            "img/proyectos/Notestack/cuerpo_Cuaderno.jpg" //img dando problema 
         ],
         descripciones: [
             "Pantalla principal de NoteStack.",
             "Vista de organización de cuadernos.",
-            "Sistema de búsqueda de notas y contenido."
-            "Gestión y edición de notas dentro de la aplicación.",
+            "Sistema de búsqueda de notas y contenido.",
+            "Gestión y edición de notas dentro de la aplicación."
         ]
     }
 };
