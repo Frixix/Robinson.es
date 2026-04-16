c69a588ea320da165d43f5b82371760df96a19a5
frix <robinsonestibenrodriguez1234@gmail.com>
2026-04-15 19:13:24 -0500

    Cambio semantico ../ por /

--- doc_js/script.js
+++ doc_js/script.js
@@ -28,10 +28,10 @@ const proyectosData = {
     inventario: {
         modalId: "modal-inventario",
         imagenes: [
-            "../img/proyectos/Inventario_2.0/inventario_inicio.jpg",
-            "../img/proyectos/Inventario_2.0/entrada_inventario.jpg",
-            "../img/proyectos/Inventario_2.0/salida_inventario.jpg",
-            "../img/proyectos/Inventario_2.0/historial_salidas.jpg"
+            "/img/proyectos/Inventario_2.0/inventario_inicio.jpg",
+            "/img/proyectos/Inventario_2.0/entrada_inventario.jpg",
+            "/img/proyectos/Inventario_2.0/salida_inventario.jpg",
+            "/img/proyectos/Inventario_2.0/historial_salidas.jpg"
         ],
         descripciones: [
             "Pantalla principal del sistema de inventario.",
@@ -44,9 +44,9 @@ const proyectosData = {
     notestack: {
         modalId: "modal-notestack",
         imagenes: [
-            "../img/proyectos/Notestack/pantalla_principal.jpg",
-            "../img/proyectos/Notestack/cuaderno.jpg",
-            "../img/proyectos/Notestack/notas.jpg",
+            "/img/proyectos/Notestack/pantalla_principal.jpg",
+            "/img/proyectos/Notestack/cuaderno.jpg",
+            "/img/proyectos/Notestack/notas.jpg",
             "../img/proyectos/Notestack/cuerpo_cuaderno.jpg"
         ],
         descripciones: [
@@ -60,10 +60,10 @@ const proyectosData = {
     solven: {
         modalId: "modal-solven",
         imagenes: [
-            "../img/proyectos/Solven/solven.jpg",
-            "../img/proyectos/Solven/pantalla-principal.jpg",
-            "../img/proyectos/Solven/area-de-transacción.jpg",
-            "../img/proyectos/Solven/filtro-categoria.jpg"
+            "/img/proyectos/Solven/solven.jpg",
+            "/img/proyectos/Solven/pantalla-principal.jpg",
+            "/img/proyectos/Solven/area-de-transacción.jpg",
+            "/img/proyectos/Solven/filtro-categoria.jpg"
         ],
         descripciones: [
             "Vista general de Solven.",
